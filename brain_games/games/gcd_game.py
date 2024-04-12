b4733f93abbcc0426491ba528288f7fa99da8d51
Igor <select12345@yandex.ru>
2024-04-12 17:39:53 +0700

    bug fix

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -8,7 +8,7 @@ def game(user_name, tries=3):
     for attempt in range(0, tries):
         num1 = random.randint(1, 100)
         num2 = random.randint(1, 100)
-        question = f'Question: {num1}, {num2}'
+        question = f'Question: {num1} {num2}'
         correct = gcd.calc_gcd(num1, num2)
         is_answer_correct = game_interface.cli(question, correct, user_name)
         if not is_answer_correct:
